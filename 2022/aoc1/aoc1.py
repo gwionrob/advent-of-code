3f814ea2818adbc556399887354a9534b1a45542
gwionrob <robertsongwion21@gmail.com>
2022-12-01 19:51:07 +0000

    advent of code 1 solution

---
+++ 2022/aoc1/aoc1.py
@@ -0,0 +1,21 @@
+"""Advent of Code 1"""
+
+with open("input", "r", encoding="utf-8") as input_file:
+
+    calories: str = input_file.read()
+
+calories_arr = calories.splitlines()
+
+elves = []
+START_OF_ELF = 0
+
+for i in range(0, len(calories_arr) - 1):
+    if calories_arr[i] == "":
+        elves.append(sum(list(map(int, calories_arr[START_OF_ELF:i]))))
+        START_OF_ELF = i + 1
+
+print(max(elves))  # solution to part 1
+
+elves.sort()
+
+print(sum(elves[-3:]))  # solution to part 2
