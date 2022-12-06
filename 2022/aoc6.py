2ac5553381a674f733a917ef4262e61d6412bedf
gwionrob <robertsongwion21@gmail.com>
2022-12-06 15:01:00 +0000

    small tidy up of day 6

--- 2022/aoc6.py
+++ 2022/aoc6.py
@@ -3,15 +3,15 @@
 with open("data/day6.txt", "r", encoding="utf-8") as input_file:
     SIGNAL = input_file.read()
     packet_marker = next(
-        SIGNAL[i : i + 4]
-        for i in range(0, len(SIGNAL) - 5)
+        i + 4
+        for i in range(len(SIGNAL) - 3)
         if len(set(SIGNAL[i : i + 4])) == len(SIGNAL[i : i + 4])
     )
     message_marker = next(
-        SIGNAL[i : i + 14]
-        for i in range(0, len(SIGNAL) - 15)
+        i + 14
+        for i in range(len(SIGNAL) - 13)
         if len(set(SIGNAL[i : i + 14])) == len(SIGNAL[i : i + 14])
     )
 
-print(SIGNAL.index(packet_marker) + 4)  # solution to part 1
-print(SIGNAL.index(message_marker) + 14)  # solution to part 2
+print(packet_marker)  # solution to part 1
+print(message_marker)  # solution to part 2
