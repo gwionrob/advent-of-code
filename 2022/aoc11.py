7370c1765e2159ce86cf294fc744cc8e09b781f3
gwionrob <robertsongwion21@gmail.com>
2022-12-20 23:39:04 +0000

    solution to aoc 11 part 1

---
+++ 2022/aoc11.py
@@ -0,0 +1,34 @@
+"""Advent of Code 11 solution"""
+from heapq import nlargest
+from math import prod
+
+with open("data/day11.txt", "r", encoding="utf-8") as input_file:
+    input_data = input_file.read().splitlines()
+    monkeys = {
+        int(input_data[i][7]): {
+            "items": [item[:2] for item in input_data[i + 1].split(" ")[4:]],
+            "operation": eval(
+                input_data[i + 2]
+                .lstrip()
+                .split(" ", 1)[1]
+                .replace("new =", "lambda old:")
+            ),
+            "test": lambda worry, item, i=i: monkeys[
+                int(input_data[i + 4].split(" ")[-1])
+            ]["items"].append(worry)
+            if worry % int(input_data[i + 3].split(" ")[-1]) == 0
+            else monkeys[int(input_data[i + 5].split(" ")[-1])]["items"].append(worry),
+        }
+        for i in range(0, len(input_data), 7)
+    }
+
+inspect_count = {monkey: 0 for monkey in monkeys}
+for _ in range(20):
+    for i in monkeys:
+        inspect_count[i] += len(monkeys[i]["items"])
+        original_length = len(monkeys[i]["items"])
+        for item in monkeys[i]["items"]:
+            worry = monkeys[i]["operation"](int(item)) // 3
+            monkeys[i]["test"](worry, item)
+        monkeys[i]["items"] = monkeys[i]["items"][original_length:]
+print(prod(nlargest(2, inspect_count.values())))  # solution to part 1
