2f6b5dddfafd6ff77c9aa6088702c4dce8243872
gwionrob <robertsongwion21@gmail.com>
2022-12-21 12:33:45 +0000

    solution to aoc11 part 2 yet to be solved

--- 2022/aoc11.py
+++ 2022/aoc11.py
@@ -2,7 +2,7 @@
 from heapq import nlargest
 from math import prod
 
-with open("data/day11.txt", "r", encoding="utf-8") as input_file:
+with open("data/day11test.txt", "r", encoding="utf-8") as input_file:
     input_data = input_file.read().splitlines()
     monkeys = {
         int(input_data[i][7]): {
@@ -18,12 +18,14 @@ with open("data/day11.txt", "r", encoding="utf-8") as input_file:
             ]["items"].append(worry)
             if worry % int(input_data[i + 3].split(" ")[-1]) == 0
             else monkeys[int(input_data[i + 5].split(" ")[-1])]["items"].append(worry),
+            "modulo": lambda item, i=i: item % int(input_data[i + 3].split(" ")[-1]),
         }
         for i in range(0, len(input_data), 7)
     }
 
 inspect_count = {monkey: 0 for monkey in monkeys}
 for _ in range(20):
+    print(_)
     for i in monkeys:
         inspect_count[i] += len(monkeys[i]["items"])
         original_length = len(monkeys[i]["items"])
@@ -32,3 +34,14 @@ for _ in range(20):
             monkeys[i]["test"](worry, item)
         monkeys[i]["items"] = monkeys[i]["items"][original_length:]
 print(prod(nlargest(2, inspect_count.values())))  # solution to part 1
+inspect_count = {monkey: 0 for monkey in monkeys}
+for _ in range(10000):
+    print(_)
+    for i in monkeys:
+        inspect_count[i] += len(monkeys[i]["items"])
+        original_length = len(monkeys[i]["items"])
+        for item in monkeys[i]["items"]:
+            worry = monkeys[i]["operation"](monkeys[i]["modulo"](int(item)))
+            monkeys[i]["test"](worry, item)
+        monkeys[i]["items"] = monkeys[i]["items"][original_length:]
+print(prod(nlargest(2, inspect_count.values())))  # solution to part 2
